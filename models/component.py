02dcf683d5477dc2f22b5e0a1439f3f7b9424e32
Andy Kim <andy.chiwon.kim@gmail.com>
2026-08-23 12:17:11 +0900

    wip

--- models/component.py
+++ models/component.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
 from scraper.tools.tools import df_krx, COMPONENTS_DIR
 from scraper.models.json_models import JsonModel, JsonModelManager, InfoSection
-from pathlib import Path
 
 class Company(BaseModel):
     # simple vehicle that carries only name and code
